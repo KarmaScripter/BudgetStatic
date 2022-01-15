ff64e24e85cca9ab19a1789bf506b1785dcb11f1
KarmaScripter <terry.eppler@gmail.com>
2022-01-15 11:16:31 -0500

    added form to StaticError class;

--- StaticError.cs
+++ StaticError.cs
@@ -1,12 +1,19 @@
-﻿// <copyright file="{ClassName}.cs" company="Terry D. Eppler">
-// Copyright (c) Eppler. All rights reserved.
+﻿// <copyright file = "StaticError.cs" company = "Terry D. Eppler">
+// Copyright (c) Terry D. Eppler. All rights reserved.
 // </copyright>
 
 namespace BudgetExecution
 {
     using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Text;
+    using System.Windows.Forms;
+    using Syncfusion.WinForms.Controls;
 
-    public partial class StaticError
+    public partial class StaticError : SfForm
     {
         /// <summary>
         /// Gets the exception.
@@ -21,12 +28,29 @@ namespace BudgetExecution
         /// </summary>
         public new string Text { get; set; }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="StaticError"/> class.
-        /// </summary>
         public StaticError()
         {
             InitializeComponent();
+
+            this.Style.TitleBar.Height = 26;
+            this.Style.TitleBar.BackColor = Color.White;
+            this.Style.TitleBar.IconBackColor = Color.FromArgb( 15, 161, 212 );
+            this.BackColor = Color.White;
+            this.Style.TitleBar.ForeColor = ColorTranslator.FromHtml( "#343434" );
+            this.Style.TitleBar.CloseButtonForeColor = Color.DarkGray;
+            this.Style.TitleBar.MaximizeButtonForeColor = Color.DarkGray;
+            this.Style.TitleBar.MinimizeButtonForeColor = Color.DarkGray;
+            this.Style.TitleBar.HelpButtonForeColor = Color.DarkGray;
+            this.Style.TitleBar.IconHorizontalAlignment = HorizontalAlignment.Left;
+
+            this.Style.TitleBar.Font = this.Font = new System.Drawing.Font( "Microsoft Sans Serif",
+                11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point,
+                ( (byte)( 0 ) ) );
+
+            this.Style.TitleBar.TextHorizontalAlignment = HorizontalAlignment.Center;
+
+            this.Style.TitleBar.TextVerticalAlignment =
+                System.Windows.Forms.VisualStyles.VerticalAlignment.Center;
         }
 
         /// <summary>
@@ -58,4 +82,5 @@ namespace BudgetExecution
             Text = message;
         }
     }
+
 }
